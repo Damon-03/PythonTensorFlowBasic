6abeba85d70b86a209e3db2ccb1bd9b693ab59a8
Damon-03 <73558661+Damon-03@users.noreply.github.com>
2021-01-08 08:09:03 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import tensorflow
-import keras
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences", "age", "goout", "romantic"]]
+
+predict = "G3"
+
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
